23feb79d01c9a3430616aa003bbd10bcb6451654
varunbw <varunbalawade@gmail.com>
2025-02-16 19:18:02 +0530

    Move function definitions outside class definition

--- src/receiver.cpp
+++ src/receiver.cpp
@@ -34,154 +34,159 @@ public:
      
         return;
     }
-
-    /*
-        Initialize the server
-        @return true if initialization is successful, false otherwise
-    */
-    bool InitializeServer() {
-        
-        // Set server address information
-        address.sin_family = AF_INET;
-        address.sin_addr.s_addr = INADDR_ANY;
-        address.sin_port = htons(serverPort);
-        
-        // Create socket file descriptor
-        if ((serverFD = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-            Log::Error("FileReceiver::InitializeServer()", "Socket creation error");
-            return false;
-        }
-
-        // Bind the socket to the port
-        if (bind(serverFD, (struct sockaddr*)&address, sizeof(address)) < 0) {
-            Log::Error("FileReceiver::InitializeServer()", "Bind error");
-            return false;
-        }
-
-        // Start listening for connections
-        if (listen(serverFD, 3) < 0) {
-            Log::Error("FileReceiver::InitializeServer()", "Listen error");
-            return false;
-        }
-
-        std::cout << "[INFO] FileReceiver::InitializeServer(): Server listening on port " << serverPort << std::endl;
-        return true;
-    }
-
-    /*
-        Accept a connection
-        @return true if connection is accepted, false otherwise
-    */
-    bool AcceptConnection() {
-        
-        clientSocket = accept(serverFD, (struct sockaddr*)&address, (socklen_t*)&addrlen);
-        
-        if (clientSocket < 0) {
-            Log::Error("FileReceiver::AcceptConnection()", "Accept error");
-            return false;
-        }
-        
-        return true;
-    }
-
-    /*
-        Receive a file from the client
-        @param filename: path to the file to save
-        @return true if file is received successfully, false otherwise
-    */
-    bool ReceiveFile(const std::string& filename) {
-        
-        std::vector<unsigned char> encryptedData;
-        std::vector<unsigned char> decryptedData;
-
-        std::string buffer(1024, '\0');
-        int bytesRead;
-
-        // Read the size of file to be received
-        size_t fileSize = -1;
-        if (read(clientSocket, &fileSize, sizeof(fileSize)) != sizeof(fileSize)) {
-            Log::Error("FileReceiver::ReceiveFile()", "Error reading file size");
-            return false;
-        }
-
-        // Read the file data based on the file size
-        size_t totalBytesRead = 0;
-        while ((totalBytesRead < fileSize) && (bytesRead = read(clientSocket, &buffer[0], buffer.size())) > 0) {
-            encryptedData.insert(encryptedData.end(), buffer.begin(), buffer.begin() + bytesRead);
-            totalBytesRead += bytesRead;
-        }
-
-        // Verify that the file data was read correctly
-        if (totalBytesRead != fileSize) {
-            // Log::Error("FileReceiver::ReceiveFile()", "File size mismatch");
-            Log::Error("FileReceiver::ReceiveFile()", std::format("File size mismatch, expected {} bytes, but read {} bytes", fileSize, totalBytesRead));
-            return false;
-        }
-        
-        // Decrypt the Data
-        bool decryptionStatus = Crypto::DecryptData(encryptedData, decryptedData, Crypto::preSharedKey, Crypto::preSharedIV);
-        if (decryptionStatus == false) {
-            Log::Error("FileReceiver::ReceiveFile()", "Decryption failed");
-            return false;
-        }
-
-        // Write decrypted Data to file
-        std::ofstream outfile(filename, std::ios::binary);
-        if (!outfile) {
-            Log::Error("FileReceiver::ReceiveFile()", std::format("Failed to create file '{}'", filename));
-            return false;
-        }
-
-        // Read hash from socket
-        std::vector<unsigned char> receivedHash(32);
-        if (read(clientSocket, receivedHash.data(), receivedHash.size()) != receivedHash.size()) {
-            Log::Error("FileReceiver::ReceiveFile()", "Error reading hash");
-            return false;
-        }
-
-        // Calculate hash of decrypted data
-        std::vector<unsigned char> hash = Crypto::CalculateHash(decryptedData);
-        if (hash.empty()) {
-            Log::Error("FileReceiver::ReceiveFile()", "Error calculating hash");
-            return false;
-        }
-
-        // Compare the received hash with the calculated hash
-        if (hash != receivedHash) {
-            Log::Error("FileReceiver::ReceiveFile()", "Hash mismatch, file contents are invalid");
-            return false;
-        }
-
-        // Write the decrypted data to the file
-        outfile.write(reinterpret_cast<const char*>(decryptedData.data()), decryptedData.size());
-        outfile.close();
-
-        Log::Success("FileReceiver::ReceiveFile()", std::format("File saved as {} successfully!", filename));
-        return true;
-    }
-
-    /*
-        Close the connection
-    */
-    void CloseConnection() {
-        
-        if (clientSocket != -1) {
-            close(clientSocket);
-            clientSocket = -1;
-        }
-
-        if (serverFD != -1) {
-            close(serverFD);
-            serverFD = -1;
-        }
-
-        return;
-    }
-
+    
     ~FileReceiver() {
         CloseConnection();
     }
+
+    bool InitializeServer();
+    bool AcceptConnection();
+    bool ReceiveFile(const std::string& filename);
+    void CloseConnection();
 };
+
+/*
+    Initialize the server
+    @return true if initialization is successful, false otherwise
+*/
+bool FileReceiver::InitializeServer() {
+        
+    // Set server address information
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(serverPort);
+    
+    // Create socket file descriptor
+    if ((serverFD = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        Log::Error("FileReceiver::InitializeServer()", "Socket creation error");
+        return false;
+    }
+
+    // Bind the socket to the port
+    if (bind(serverFD, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        Log::Error("FileReceiver::InitializeServer()", "Bind error");
+        return false;
+    }
+
+    // Start listening for connections
+    if (listen(serverFD, 3) < 0) {
+        Log::Error("FileReceiver::InitializeServer()", "Listen error");
+        return false;
+    }
+
+    std::cout << "[INFO] FileReceiver::InitializeServer(): Server listening on port " << serverPort << std::endl;
+    return true;
+}
+
+/*
+    Accept a connection
+    @return true if connection is accepted, false otherwise
+*/
+bool FileReceiver::AcceptConnection() {
+    
+    clientSocket = accept(serverFD, (struct sockaddr*)&address, (socklen_t*)&addrlen);
+    
+    if (clientSocket < 0) {
+        Log::Error("FileReceiver::AcceptConnection()", "Accept error");
+        return false;
+    }
+    
+    return true;
+}
+
+/*
+    Receive a file from the client
+    @param filename: path to the file to save
+    @return true if file is received successfully, false otherwise
+*/
+bool FileReceiver::ReceiveFile(const std::string& filename) {
+    
+    std::vector<unsigned char> encryptedData;
+    std::vector<unsigned char> decryptedData;
+
+    std::string buffer(1024, '\0');
+    int bytesRead;
+
+    // Read the size of file to be received
+    size_t fileSize = -1;
+    if (read(clientSocket, &fileSize, sizeof(fileSize)) != sizeof(fileSize)) {
+        Log::Error("FileReceiver::ReceiveFile()", "Error reading file size");
+        return false;
+    }
+
+    // Read the file data based on the file size
+    size_t totalBytesRead = 0;
+    while ((totalBytesRead < fileSize) && (bytesRead = read(clientSocket, &buffer[0], buffer.size())) > 0) {
+        encryptedData.insert(encryptedData.end(), buffer.begin(), buffer.begin() + bytesRead);
+        totalBytesRead += bytesRead;
+    }
+
+    // Verify that the file data was read correctly
+    if (totalBytesRead != fileSize) {
+        // Log::Error("FileReceiver::ReceiveFile()", "File size mismatch");
+        Log::Error("FileReceiver::ReceiveFile()", std::format("File size mismatch, expected {} bytes, but read {} bytes", fileSize, totalBytesRead));
+        return false;
+    }
+    
+    // Decrypt the Data
+    bool decryptionStatus = Crypto::DecryptData(encryptedData, decryptedData, Crypto::preSharedKey, Crypto::preSharedIV);
+    if (decryptionStatus == false) {
+        Log::Error("FileReceiver::ReceiveFile()", "Decryption failed");
+        return false;
+    }
+
+    // Read hash sent by sender
+    std::vector<unsigned char> receivedHash(32);
+    if (read(clientSocket, receivedHash.data(), receivedHash.size()) != receivedHash.size()) {
+        Log::Error("FileReceiver::ReceiveFile()", "Error reading hash");
+        return false;
+    }
+
+    // Calculate hash of decrypted data
+    std::vector<unsigned char> hash = Crypto::CalculateHash(decryptedData);
+    if (hash.empty()) {
+        Log::Error("FileReceiver::ReceiveFile()", "Error calculating hash");
+        return false;
+    }
+
+    // Compare the received hash with the calculated hash
+    if (hash != receivedHash) {
+        Log::Error("FileReceiver::ReceiveFile()", "Hash mismatch, file contents are invalid");
+        return false;
+    }
+
+    // Write decrypted Data to file
+    std::ofstream outfile(filename, std::ios::binary);
+    if (!outfile) {
+        Log::Error("FileReceiver::ReceiveFile()", std::format("Failed to create file '{}'", filename));
+        return false;
+    }
+
+    // Write the decrypted data to the file
+    outfile.write(reinterpret_cast<const char*>(decryptedData.data()), decryptedData.size());
+    outfile.close();
+
+    Log::Success("FileReceiver::ReceiveFile()", std::format("File saved as {} successfully!", filename));
+    return true;
+}
+
+/*
+    Close the connection
+*/
+void FileReceiver::CloseConnection() {
+    
+    if (clientSocket != -1) {
+        close(clientSocket);
+        clientSocket = -1;
+    }
+
+    if (serverFD != -1) {
+        close(serverFD);
+        serverFD = -1;
+    }
+
+    return;
+}
 
 int main() {
 
--- src/sender.cpp
+++ src/sender.cpp
@@ -9,7 +9,6 @@
 #include "../include/crypto.hpp"
 #include "../include/logger.hpp"
 
-// todo move functions outside class definition
 class FileSender {
 private:
     // Sender (client) socket
@@ -32,118 +31,124 @@ public:
         return;
     }
 
-    /*
-        Connect to the server
-        @return true if connection is successful, false otherwise
-    */
-    bool ConnectToServer() {
-        
-        // Set server information
-        serverAddr.sin_family = AF_INET;
-        serverAddr.sin_port = htons(serverPort);
-
-        // Create a socket - IPv4, TCP
-        socketFD = socket(AF_INET, SOCK_STREAM, 0);
-        if (socketFD < 0) {
-            Log::Error("FileSender::ConnectToServer()", "Socket creation error");
-            return false;
-        }
-
-        if (inet_pton(AF_INET, serverIP.c_str(), &serverAddr.sin_addr) <= 0) {
-            Log::Error("FileSender::ConnectToServer()", "Invalid address/Address not supported");
-            return false;
-        }
-
-        if (connect(socketFD, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
-            Log::Error("FileSender::ConnectToServer()", "Connection failed");
-            return false;
-        }
-
-        return true;
-    }
-
-    /*
-        Send a file to the server
-        @param filename: path to the file to send
-        @return true if file is sent successfully, false otherwise
-    */
-    bool SendFile(const std::string& filename) {
-        
-        // Open file in binary mode
-        std::ifstream infile(filename, std::ios::binary);
-        if (infile.fail()) {
-            Log::Error("FileSender::SendFile()", std::format("Failed to open file '{}'", filename));
-            return false;
-        }
-
-        // Load the file contents into a vector
-        std::vector<unsigned char> plainFileData((std::istreambuf_iterator<char>(infile)), std::istreambuf_iterator<char>());
-
-        // Encrypt the file contents
-        std::vector<unsigned char> encryptedData;
-        bool encryptionStatus = Crypto::EncryptData(plainFileData, encryptedData, Crypto::preSharedKey, Crypto::preSharedIV);
-        if (encryptionStatus == false) {
-            Log::Error("FileSender::SendFile()", "Error encrypting file");
-            return false;
-        }
-
-        // Send size of file to server, so it knows how much data to expect
-        size_t fileSize = encryptedData.size();
-        if (send(socketFD, &fileSize, sizeof(fileSize), 0) < 0) {
-            Log::Error("FileSender::SendFile()", "Error sending file size");
-            return false;
-        }
-        
-        // Send the encrypted file contents to the server in 1024 byte chunks
-        size_t sentSize = 0;
-        size_t totalSize = encryptedData.size();
-        while (sentSize < totalSize) {
-            // Send the min amongst 1024 bytes, or how much ever is left to send
-            size_t chunkSize = std::min(static_cast<size_t>(1024), totalSize - sentSize);
-
-            if (send(socketFD, encryptedData.data() + sentSize, chunkSize, 0) < 0) {
-                Log::Error("FileSender::SendFile()", "Error sending encrypted file");
-                return false;
-            }
-
-            sentSize += chunkSize;
-        }
-
-        // todo move hash sending to another function
-        // Calculate hash of the file
-        std::vector<unsigned char> hash = Crypto::CalculateHash(plainFileData);
-        if (hash.empty()) {
-            Log::Error("FileSender::SendFile()", "Error calculating hash");
-            return false;
-        }
-
-        // Send hash data to server
-        if (send(socketFD, hash.data(), hash.size(), 0) < 0) {
-            Log::Error("FileSender::SendFile()", "Error sending hash");
-            return false;
-        }
-
-        Log::Success("FileSender::SendFile()", std::format("File {} sent successfully!", filename));
-        return true;
-    }
-
-    /*
-        Close the connection
-    */
-    void CloseConnection() {
-
-        if (socketFD != -1) {
-            close(socketFD);
-            socketFD = -1;
-        }
-
-        return;
-    }
-
     ~FileSender() {
         CloseConnection();
     }
+
+    bool ConnectToServer();
+    bool SendFile(const std::string& fileName);
+    void CloseConnection();
 };
+
+
+/*
+    Connect to the server
+    @return true if connection is successful, false otherwise
+*/
+bool FileSender::ConnectToServer() {
+        
+    // Set server information
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(serverPort);
+
+    // Create a socket - IPv4, TCP
+    socketFD = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketFD < 0) {
+        Log::Error("FileSender::ConnectToServer()", "Socket creation error");
+        return false;
+    }
+
+    if (inet_pton(AF_INET, serverIP.c_str(), &serverAddr.sin_addr) <= 0) {
+        Log::Error("FileSender::ConnectToServer()", "Invalid address/Address not supported");
+        return false;
+    }
+
+    if (connect(socketFD, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
+        Log::Error("FileSender::ConnectToServer()", "Connection failed");
+        return false;
+    }
+
+    return true;
+}
+
+/*
+    Send a file to the server
+    @param filename: path to the file to send
+    @return true if file is sent successfully, false otherwise
+*/
+bool FileSender::SendFile(const std::string& filename) {
+    
+    // Open file in binary mode
+    std::ifstream infile(filename, std::ios::binary);
+    if (infile.fail()) {
+        Log::Error("FileSender::SendFile()", std::format("Failed to open file '{}'", filename));
+        return false;
+    }
+
+    // Load the file contents into a vector
+    std::vector<unsigned char> plainFileData((std::istreambuf_iterator<char>(infile)), std::istreambuf_iterator<char>());
+
+    // todo move encryption to another function
+    // Encrypt the file contents
+    std::vector<unsigned char> encryptedData;
+    bool encryptionStatus = Crypto::EncryptData(plainFileData, encryptedData, Crypto::preSharedKey, Crypto::preSharedIV);
+    if (encryptionStatus == false) {
+        Log::Error("FileSender::SendFile()", "Error encrypting file");
+        return false;
+    }
+
+    // Send size of file to server, so it knows how much data to expect
+    size_t fileSize = encryptedData.size();
+    if (send(socketFD, &fileSize, sizeof(fileSize), 0) < 0) {
+        Log::Error("FileSender::SendFile()", "Error sending file size");
+        return false;
+    }
+    
+    // Send the encrypted file contents to the server in 1024 byte chunks
+    size_t sentSize = 0;
+    size_t totalSize = encryptedData.size();
+    while (sentSize < totalSize) {
+        // Send the min amongst 1024 bytes, or how much ever is left to send
+        size_t chunkSize = std::min(static_cast<size_t>(1024), totalSize - sentSize);
+
+        if (send(socketFD, encryptedData.data() + sentSize, chunkSize, 0) < 0) {
+            Log::Error("FileSender::SendFile()", "Error sending encrypted file");
+            return false;
+        }
+
+        sentSize += chunkSize;
+    }
+
+    // todo move hash sending to another function
+    // Calculate hash of the file
+    std::vector<unsigned char> hash = Crypto::CalculateHash(plainFileData);
+    if (hash.empty()) {
+        Log::Error("FileSender::SendFile()", "Error calculating hash");
+        return false;
+    }
+
+    // Send hash data to server
+    if (send(socketFD, hash.data(), hash.size(), 0) < 0) {
+        Log::Error("FileSender::SendFile()", "Error sending hash");
+        return false;
+    }
+
+    Log::Success("FileSender::SendFile()", std::format("File {} sent successfully!", filename));
+    return true;
+}
+
+/*
+    Close the connection
+*/
+void FileSender::CloseConnection() {
+
+    if (socketFD != -1) {
+        close(socketFD);
+        socketFD = -1;
+    }
+
+    return;
+}
 
 
 int main(void) {
@@ -153,9 +158,9 @@ int main(void) {
 
     FileSender sender(IP, port);
 
-    if (!sender.ConnectToServer())
+    if (sender.ConnectToServer() == false)
         return 1;
-    if (!sender.SendFile("../temp/file_to_send.txt"))
+    if (sender.SendFile("../temp/file_to_send.txt") == false)
         return 1;
 
     return 0;
