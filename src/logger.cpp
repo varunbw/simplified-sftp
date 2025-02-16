b7e36a352d3f5bfe529898729773fa847a508bb9
varunbw <varunbalawade@gmail.com>
2025-02-16 18:46:57 +0530

    Move logger function definitions to their own source file

---
+++ src/logger.cpp
@@ -0,0 +1,31 @@
+#include "../include/logger.hpp"
+
+namespace Log {
+    void Error(const std::string& functionName, const std::string& message) {
+        std::cerr << std::format(
+            RED_START "[ERROR] {}: {}\n" RESET_COLOR,
+            functionName, message
+        );
+    }
+
+    void Success(const std::string& functionName, const std::string& message) {
+        std::cout << std::format(
+            GREEN_START "[INFO] {}: {}\n" RESET_COLOR,
+            functionName, message
+        );
+    }
+
+    void Info(const std::string& functionName, const std::string& message) {
+        std::cout << std::format(
+            "[INFO] {}: {}\n",
+            functionName, message
+        );
+    }
+
+    void Warning(const std::string& functionName, const std::string& message) {
+        std::cout << std::format(
+            YELLOW_START "[WARNING] {}: {}\n" RESET_COLOR,
+            functionName, message
+        );
+    }
+}
